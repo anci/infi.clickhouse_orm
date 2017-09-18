b284465f0e266cfa8e6a60b9d9c2dceb27b1e998
anci <mad7000@gmail.com>
2017-09-18 23:32:50 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, print_function
+
+from setuptools import setup
+
+
+setup(name='funniest',
+    version='0.9.2',
+    url='https://github.com/anci/infi.clickhouse_orm',
+    packages=['src'],
+    zip_safe=False)
