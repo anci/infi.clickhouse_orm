6570264ffdd6e0aa39c2fc1d281df6a74030f1cb
anci <mad7000@gmail.com>
2017-10-02 22:54:02 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,26 @@ from __future__ import unicode_literals, print_function
 from setuptools import setup, find_packages
 
 
-setup(name='infi.clickhouse_orm',
-    version='0.9.7',
-    packages=find_packages('src'),
-    zip_safe=False)
+if __name__ == '__main__':
+    SETUP_INFO = dict(
+        name='infi.clickhouse_orm',
+        version='0.9.7',
+        license='BSD',
+        install_requires=[
+            'pytz',
+	        'requests',
+	        'setuptools',
+	        'six'
+	    ],
+        namespace_packages=['infi'],
+        package_dir={'': 'src'},
+        package_data={'': []},
+        include_package_data=True,
+        zip_safe=False,
+        entry_points = dict(
+            console_scripts=[],
+            gui_scripts=[],
+        ),
+    )
+    SETUP_INFO['packages'] = find_packages('src')
+    setup(**SETUP_INFO)
